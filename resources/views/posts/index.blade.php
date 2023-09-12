5f9ce172f1ee00a6736194b2b53edc9e45dcbf96
Takuya-Fu/git <takuya0713ioc@gmail.com>
2023-09-12 22:29:34 +0900

    0912

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,5 +2,6 @@
 
 @section('content')
 <h2>機能を実装していきましょう。</h2>
-
+<h1><a href="/top"><img class="bgc" src="{{asset('images/atlas.png')}}" alt=""></a></h1>
+{{-- asset関数はpublicフォルダを参照する --}}
 @endsection
