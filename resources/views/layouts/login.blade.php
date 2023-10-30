0d7c3ce9dd0c86945fb3663df70507bf3c837ed6
Takuya-Fu <134198338+Takuya-Fu@users.noreply.github.com>
2023-10-30 22:57:07 +0900

    1030

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<meta charset="utf-8" />
+    <meta charset="utf-8" />
     <!--IEブラウザ対策-->
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="description" content="ページの内容を表す文章" />
@@ -19,23 +20,27 @@
     <link rel="apple-touch-icon-precomposed" href="画像のURL" />
     <!--OGPタグ/twitterカード-->
 </head>
+
 <body>
     <header>
         <div id = "head" class= "header_wrapper">
-            <h1><a href="/top"><img class="header_logo" src="{{asset('images/atlas.png')}}" alt=""></a></h1>
+            <h1><a href="/top"><img class="header_logo" src="{{ asset('images/atlas.png') }}" alt=""></a>
+            </h1>
             <div id="head_inner" class="header_inner">
                 <div class="header_right">
-                        <div id="login_name" class="login_name">
-                            <p>〇〇さん</p>
-                        </div>
+                    <div id="login_name" class="login_name">
+                        <p>〇〇さん</p>
+                    </div>
                 </div>
                 {{-- アコーディオンメニュー部分→head_innerで100%だと同じ広さになるかも --}}
                 <nav class="ac-item">
-                    <ul class="ac__body">
-                        <li class="body_inner"><a href="/top">HOME</a></li>
-                        <li class="body_inner"><a href="/profile">プロフィール編集</a></li>
-                        <li class="body_inner"><a href="/logout">ログアウト</a></li>
-                    </ul>
+                    <div class="ac__button">
+                        <ul class="ac__body">
+                            <li class="body_inner"><a href="/top">HOME</a></li>
+                            <li class="body_inner"><a href="/profile">プロフィール編集</a></li>
+                            <li class="body_inner"><a href="/logout">ログアウト</a></li>
+                        </ul>
+                    </div>
                 </nav>
                 {{-- //アコーディオンメニュー部分 --}}
                 <div class="icon_wrapper">
@@ -53,13 +58,13 @@
             <div id="confirm">
                 <p>〇〇さんの</p>
                 <div>
-                <p>フォロー数</p>
-                <p>〇〇名</p>
+                    <p>フォロー数</p>
+                    <p>〇〇名</p>
                 </div>
                 <p class="btn"><a href="">フォローリスト</a></p>
                 <div>
-                <p>フォロワー数</p>
-                <p>〇〇名</p>
+                    <p>フォロワー数</p>
+                    <p>〇〇名</p>
                 </div>
                 <p class="btn"><a href="">フォロワーリスト</a></p>
             </div>
@@ -68,6 +73,7 @@
     </div>
     <footer>
     </footer>
-    <script src="{{asset('/js/accordion.js')}}"></script>
+    <script src="{{ asset('/js/accordion.js') }}"></script>
 </body>
+
 </html>
