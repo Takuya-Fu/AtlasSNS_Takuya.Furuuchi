adf049aadaa6fa7f89197e3590ffe4f3b961b775
Takuya-Fu <134198338+Takuya-Fu@users.noreply.github.com>
2023-11-18 15:40:01 +0900

    1118

--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -6,9 +6,10 @@
 {{-- @extends('posts.index') --}}
 {{-- postsフォルダ内のindex.blade.phpの内容を表示する --}}
 @section('content')
-    {{-- contentとして変数にデータを渡す部分のこと --}}
-    <h2>ユーザー検索</h2>
     <div id="main" class="main_container">
-        <input class="post" type="text" placeholder="ユーザー名" size="100%" height="20%">
+        <div class="search-wrapper">
+            <input class="user_search" type="text" placeholder="ユーザー名" size="100%" height="20%">
+            <img class="user_search_icon" src="{{ asset('images/search.png') }}" alt="ユーザーを検索">
+        </div>
     </div>
 @endsection
