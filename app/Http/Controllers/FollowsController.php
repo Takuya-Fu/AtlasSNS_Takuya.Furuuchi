71681f4115d118dfd0d008e70d9484b24c3a28a6
Takuya-Fu <134198338+Takuya-Fu@users.noreply.github.com>
2024-01-21 23:08:15 +0900

    フォロー・フォロワー機能作成

--- app/Http/Controllers/FollowsController.php
+++ app/Http/Controllers/FollowsController.php
@@ -2,9 +2,13 @@
 // 0121作成
 // https://teratail.com/questions/297833
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
+use App\User;
+// User.phpからユーザーモデルを引用
+use App\Post;
+// Post.phpからポスト（投稿）モデルを引用
+use App\Follow;
+// （未作成）Follow.phpからフォローモデルを引用
 class FollowsController extends Controller
 {
     //
